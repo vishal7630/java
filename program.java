bf716bbb545f99f9b051cd47ad95bea765aaa129
Vishal Kumar Ray <vishalkhonsa74@gmail.com>
2025-08-06 17:00:14 +0530

    Add files via upload

---
+++ program.java
@@ -0,0 +1,9 @@
+public class program2 {
+    public static void main(String[] args) {
+        
+        System.out.print("My address is Alpha-1\nBlock e\nGreater noida");
+        System.out.println("My address is Block e");
+        System.out.println("Greater noida");
+        
+    }
+}
---
+++ program2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner Sc = new Scanner(System.in);
+        int age = Sc. nextInt();
+        System.out.println("My age is" + age);
+        
+    }
+}
